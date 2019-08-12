06dfa797000f9aeb4eeb3ca70aa4a55c6efbf6f7
yu0217 <noseyuu0217@gmail.com>
2019-08-12 18:18:41 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  root   'static_pages#home'
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :users
-  resources :account_activations, only: [:edit]
+  root 'static_pages#home'
+  get     '/help',    to: 'static_pages#help'
+  get     '/about',   to: 'static_pages#about'
+  get     '/contact', to: 'static_pages#contact'
+  get     '/signup',  to: 'users#new'
+  post    '/signup',  to: 'users#create'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+  resources :users                                                              # usersリソースをRESTfullな構造にするためのコード。
+  resources :account_activations, only: [:edit]                                 # editアクションのみaccount_activationsリソースを適用
+  resources :password_resets,     only: [:new, :create, :edit, :update]         # password再設定用のリソースを適用
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,6 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # https://17d17c5e4b3845169c1baf3e4ee05911.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
